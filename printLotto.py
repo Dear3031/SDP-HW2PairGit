e1b2f0fdab9c0b8a413c72b808d59895155b2558
DearPhanu <ki.phanu_st@tni.ac.th>
2016-12-16 03:42:41 +0000

    First commit

--- printLotto.py
+++ printLotto.py
@@ -2,6 +2,17 @@ from random import randint
 
 lottorecieve = input("Enter your Huay : ")
 
-
 lottorecieve+=randint(0,9)
-print(lottorecieve)
+print("\nFisrt lottorecieve is %d" %lottorecieve)
+
+print("\nLast three number lottorecieve is... ")
+for i in range(4):
+    temp = randint(0,999)
+    if(temp<100):
+        print("0%d" %temp)
+    elif(temp<10):
+        print("00%d" %temp)
+    else:
+        print temp
+        
+    
